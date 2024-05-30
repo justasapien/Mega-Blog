f285e3bb8e8bd4a065cb90112bae80b9f845b468
just_a_sapien <victorysharma07@gmail.com>
2024-05-31 02:02:46 +0530

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width='20px'}) {
+  return (
+    <div>
+      <img className='bg-black-400 w-20 rounded-full' src="../../public/LogoMega.png" alt="" />
+    </div>
+  )
+}
+
+export default Logo
